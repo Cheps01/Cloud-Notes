9cdd67528a9ea6f1522ddbec305e84e8d8a1c2ed
Cheps01 <josecarlosgordilloa@hotmail.com>
2025-11-02 18:25:30 -0600

    Minor update

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,7 @@ import authRoutes from "./routes/auth.routes.js"
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 // Middleware
 app.use(cors())
